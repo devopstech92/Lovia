137a0f000de836626c17bca47e53ee7e26745283
DevopsTECH92 <devopstech1992@gmail.com>
2025-03-27 09:00:00 +0200

    Update lovia_interpreter.cpp

--- lovia_interpreter.cpp
+++ lovia_interpreter.cpp
@@ -1,69 +1,100 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 #include <iostream>
-#include <string>
-#include <unordered_map>
-#include <vector>
 #include <sstream>
+#include "lovia_interpreter.h"
 
 using namespace std;
 
-class LoviaInterpreter {
+class LoviaGUI {
 private:
-    unordered_map<string, int> variables;
+    sf::RenderWindow window;
+    sf::Font font;
+    sf::Text inputText;
+    sf::Text outputText;
+    sf::RectangleShape inputBox;
+    sf::RectangleShape runButton;
+    sf::Text runButtonText;
+    string userInput;
+    LoviaInterpreter interpreter;
 
 public:
-    void execute(const vector<string>& lines) {
-        for (size_t i = 0; i < lines.size(); i++) {
-            string line = lines[i];
-            stringstream ss(line);
-            string command;
-            ss >> command;
+    LoviaGUI() : window(sf::VideoMode(600, 400), "Lovia Interpreter") {
+        if (!font.loadFromFile("arial.ttf")) {
+            cerr << "Error loading font!" << endl;
+        }
 
-            if (command == "say") {
-                string output;
-                getline(ss, output);
-                output = output.substr(1); // Remove leading space
-                cout << output << endl;
-            }
-            else if (command == "set") {
-                string varName, to;
-                int value;
-                ss >> varName >> to >> value;
-                if (to == "to") {
-                    variables[varName] = value;
+        inputBox.setSize(sf::Vector2f(500, 40));
+        inputBox.setPosition(50, 50);
+        inputBox.setFillColor(sf::Color(200, 200, 200));
+
+        inputText.setFont(font);
+        inputText.setCharacterSize(20);
+        inputText.setFillColor(sf::Color::Black);
+        inputText.setPosition(60, 60);
+
+        runButton.setSize(sf::Vector2f(100, 40));
+        runButton.setPosition(250, 120);
+        runButton.setFillColor(sf::Color(100, 200, 100));
+
+        runButtonText.setFont(font);
+        runButtonText.setCharacterSize(20);
+        runButtonText.setFillColor(sf::Color::Black);
+        runButtonText.setString("Run");
+        runButtonText.setPosition(275, 130);
+
+        outputText.setFont(font);
+        outputText.setCharacterSize(20);
+        outputText.setFillColor(sf::Color::White);
+        outputText.setPosition(50, 200);
+    }
+
+    void run() {
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) {
+                    window.close();
+                } else if (event.type == sf::Event::TextEntered) {
+                    if (event.text.unicode < 128) {
+                        if (event.text.unicode == 8 && !userInput.empty()) {
+                            userInput.pop_back();
+                        } else if (event.text.unicode != 8) {
+                            userInput += static_cast<char>(event.text.unicode);
+                        }
+                        inputText.setString(userInput);
+                    }
+                } else if (event.type == sf::Event::MouseButtonPressed) {
+                    if (event.mouseButton.button == sf::Mouse::Left) {
+                        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                        if (runButton.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+                            stringstream ss(userInput);
+                            vector<string> lines;
+                            string line;
+                            while (getline(ss, line)) {
+                                lines.push_back(line);
+                            }
+                            interpreter.execute(lines);
+                            outputText.setString("Executed successfully!");
+                        }
+                    }
                 }
             }
-            else if (command == "repeat") {
-                int times;
-                string temp;
-                ss >> times >> temp;
-                if (temp == "times:") {
-                    vector<string> loopBody;
-                    i++; // Move to next line
-                    while (i < lines.size() && lines[i] != ":end") {
-                        loopBody.push_back(lines[i]);
-                        i++;
-                    }
-                    for (int j = 0; j < times; j++) {
-                        execute(loopBody);
-                    }
-                }
-            }
+
+            window.clear(sf::Color(50, 50, 50));
+            window.draw(inputBox);
+            window.draw(inputText);
+            window.draw(runButton);
+            window.draw(runButtonText);
+            window.draw(outputText);
+            window.display();
         }
     }
 };
 
 int main() {
-    vector<string> code = {
-        "set x to 10",
-        "say Hello, Lovia!",
-        "repeat 3 times:",
-        "    say Loop iteration!",
-        ":end",
-        "say Goodbye!"
-    };
-    
-    LoviaInterpreter interpreter;
-    interpreter.execute(code);
-    
+    LoviaGUI gui;
+    gui.run();
     return 0;
 }
